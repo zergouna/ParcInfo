1115363f6abe5bf03f50863461dfc23a9aa92b1d
Zebiri <118070500+zergouna@users.noreply.github.com>
2024-01-13 10:46:15 +0100

    Create Materiel.java

---
+++ Materiel.java
@@ -0,0 +1,28 @@
+import java.io.Serializable;
+
+class Materiel implements Serializable {
+    private String numeroSerie;
+    private String nom;
+
+    public Materiel(String numeroSerie, String nom) {
+        this.numeroSerie = numeroSerie;
+        this.nom = nom;
+    }
+
+    public String getNumeroSerie() {
+        return numeroSerie;
+    }
+
+    public String getNom() {
+        return nom;
+    }
+
+    public void setNom(String nom) {
+        this.nom = nom;
+    }
+
+    @Override
+    public String toString() {
+        return "Numéro de série : " + numeroSerie + ", Nom : " + nom;
+    }
+}
